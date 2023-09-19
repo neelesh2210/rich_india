54994e4ffce31fddf132faaf75be6281303e23ec
neelesh2210 <neelesh2210@gmail.com>
2023-09-19 18:47:16 +0530

    profile image download

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -117,7 +117,7 @@
                                                         Website
                                                     @endif
                                                         <br>
-                                                    <b><a href="{{asset('frontend/images/avatar/'.$user->userDetail->avatar)}}" download> <i class="fas fa-download"></i> Download Image</a> </b><br>
+                                                    <b>@if(!empty($user->avatar))<a href="{{asset('frontend/images/avatar/'.$user->avatar)}}" download> <i class="fas fa-download"></i> Download Image</a>@endif </b><br>
                                                 </td>
                                                 <td>
                                                     <b>Name: </b>{{ optional($user->sponsorDetail)->name }} <br>
