1ca86f40c01c9ddaea60d4ec02ef87f9e8719b2f
shaileshhq@gmail.com <shaileshhq@gmail.com>
2023-09-25 17:41:02 +0530

    updte signin

--- resources/views/frontend/signin.blade.php
+++ resources/views/frontend/signin.blade.php
@@ -31,13 +31,28 @@
                                                     @endif
                                                 </div>
                                                 <div class="form-group">
+                                                    <label for="password">Password</label>
+                                                    <div class="input-group" id="show_hide_password">
+                                                        <input type="password" class="form-control" name="password" id="password"
+                                                            value="" placeholder="Enter Password..." autocomplete="new-password">
+                                                        <div class="input-group-append">
+                                                            <div class="input-group-text">
+                                                                <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    @if ($errors->has('password'))
+                                                        <div class="text-danger lbl_msg">{{ $errors->first('password') }}</div>
+                                                    @endif
+                                                </div>
+                                                {{-- <div class="form-group">
                                                     <label class="form-control-label">Password</label>
                                                     <input type="password" class="form-control" name="password" id="password"
                                                     value="" placeholder="Enter your Password" autocomplete="new-password">
                                                     @if ($errors->has('password'))
                                                     <div class="text-danger lbl_msg">{{ $errors->first('password') }}</div>
                                                      @endif
-                                                </div>
+                                                </div> --}}
                                             </div>
                                             <div class="fpsw-sec d-flex">
                                                 <div class="forgot">
@@ -63,4 +78,21 @@
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+        $("#show_hide_password a").on('click', function(event) {
+            event.preventDefault();
+            if ($('#show_hide_password input').attr("type") == "text") {
+                $('#show_hide_password input').attr('type', 'password');
+                $('#show_hide_password i').addClass("fa-eye-slash");
+                $('#show_hide_password i').removeClass("fa-eye");
+            } else if ($('#show_hide_password input').attr("type") == "password") {
+                $('#show_hide_password input').attr('type', 'text');
+                $('#show_hide_password i').removeClass("fa-eye-slash");
+                $('#show_hide_password i').addClass("fa-eye");
+            }
+        });
+    });
+</script>
 @endsection
