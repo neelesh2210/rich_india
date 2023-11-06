1246cfd1705967463b5becc3c50bb70b75ebeec7
shaileshhq@gmail.com <shaileshhq@gmail.com>
2023-11-06 16:15:30 +0530

    updte payout

--- resources/views/email/payout.blade.php
+++ resources/views/email/payout.blade.php
@@ -1,90 +1,79 @@
 <!doctype html>
 <html lang="en-US">
-  <head>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-    <title>RichIND</title>
-    <meta name="description" content="RichIND">
-  </head>
-  <style>
-    a:hover {
-      text-decoration: underline !important;
-    }
-  </style>
-  <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px;" leftmargin="0">
-    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8" style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
-      <tr>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Skill Achiever</title>
+<meta name="description" content="Skill Achiever">
+</head>
+<style>
+a:hover {
+    text-decoration: underline !important;
+}
+</style>
+
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px;" leftmargin="0">
+<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+    style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+    <tr>
         <td>
-          <table style="background-color: #fff;" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-            <tr>
-              <td style="height:50px;">&nbsp;</td>
-            </tr>
-            <tr>
-              <td>
-                <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" style="max-width:650px; background-image: url({{asset('frontend/images/email-bg.jpg')}}); background-size:contain; border-radius:3px;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);padding:0 10px;">
-                  <tbody>
-                    <tr>
-                      <td>
-                        <div style="margin:0px auto;max-width:650px">
-                          <div style="line-height:0;font-size:0">
-                            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%">
-                              <tbody>
-                                <tr>
-                                  <td style="direction:ltr;font-size:0px;padding:24px 10px 48px 10px;padding-bottom:48px;padding-left:10px;padding-right:10px;padding-top:24px;text-align:center">
-                                    <div style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%">
-                                      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top" width="100%">
-                                        <tbody>
-                                          <tr>
-                                            <td style="font-size:0px;word-break:break-word">
-                                              <div style="height:96px;line-height:96px"> </div>
-                                            </td>
-                                          </tr>
-                                          <tr>
-                                            <td align="left" style="font-size:0px;padding:10px 25px;padding-top:0px;padding-bottom:0px;word-break:break-word">
-                                              <div style="font-family:Arial,sans-serif;font-size:20px;letter-spacing:normal;line-height:1;text-align:left;color:#000000">
-                                                <p style="line-height:20px;text-align:center;margin:10px 0;margin-top:25px"><span style="color:#ffffff;font-size:20px;letter-spacing:2px"><b>Congratulations,</b></span></p>
-                                                <p style="line-height:20px;text-align:center;margin:10px 0;margin-bottom:10px"><span style="color:#ffffff;font-size:14px;letter-spacing:2px"><b>Withdrawal successful on RichInd!</b></span></p>
-                                              </div>
-                                            </td>
-                                          </tr>
-                                          <tr>
-                                            <td style="font-size:0px;word-break:break-word">
-                                              <div style="height:36px;line-height:36px"> </div>
-                                            </td>
-                                          </tr>
-                                          <tr>
-                                            <td align="left" style="background:#ffffff;font-size:0px;padding:24px 24px 24px 24px;padding-top:24px;padding-right:10px;padding-bottom:24px;padding-left:10px;word-break:break-word">
-                                              <div style="font-family:Arial,sans-serif;font-size:16px;letter-spacing:normal;line-height:1;text-align:left;color:#000000">
-                                                <p style="line-height:24px;margin:10px 0;margin-top:10px"><span style="font-family:Poppins,Arial,Helvetica,sans-serif;font-size:16px">Greetings  @isset($user_name)  {{$user_name}} @endisset,</span></p>
-                                                <p style="line-height:24px;margin:10px 0"><span style="font-family:Poppins,Arial,Helvetica,sans-serif;font-size:16px">₹ @isset($amount){{$amount}}@endisset Withdrawal Successfully.</span><br>&nbsp; </p>
-                                                <p style="line-height:24px;margin:10px 0"><span style="font-family:Poppins,Arial,Helvetica,sans-serif;font-size:16px">We are happy to inform you that we have transferred your payment into your Bank Account</span><br><span style="font-family:Poppins,Arial,Helvetica,sans-serif;font-size:16px"></span></p>
-                                                <p style="line-height:24px;margin:10px 0"><span style="font-family:Poppins,Arial,Helvetica,sans-serif;font-size:16px">Date : {{date('d-m-Y')}}</span></p>
-                                                <p style="line-height:24px;margin:10px 0"> &nbsp;</p>
-                                                <p style="line-height:24px;margin:10px 0"><span style="font-family:Poppins,Arial,Helvetica,sans-serif;font-size:16px">Regards,</span></p>
-                                                <p style="line-height:24px;margin:10px 0;margin-bottom:10px"><span style="font-family:Poppins,Arial,Helvetica,sans-serif;font-size:16px">Team RICHIND.</span></p>
-                                              </div>
-                                            </td>
-                                          </tr>
-                                          {{-- <tr>
-                                            <td align="center" style="background:#3759e0;font-size:0px;padding:10px 25px 10px 25px;padding-right:25px;padding-left:25px;word-break:break-word"></td>
-                                          </tr> --}}
-                                        </tbody>
-                                      </table>
-                                    </div>
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          </table>
+            <table style="background-color: #fff;" width="100%" border="0"
+                align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td style="height:30px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                            style="max-width:600px; background-image:url('{{ asset('frontend/images/email-bgs.jpg') }}'); border-radius:3px;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);padding:0 40px;">
+                            <tr>
+                                <td style="height:40px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center; border-bottom:1px solid #f2e8dc; padding-bottom: 10px;">
+                                    <a href="{{ asset('frontend/images/logo-2.png') }}" title="logo" target="_blank">
+                                        <img src="{{ asset('frontend/images/logo-2.png') }}" title="logo" alt="logo" style="height: 70px">
+                                    </a>
+                                </td>
+                            </tr>
+                            <!-- Title -->
+                            <tr>
+                                <td style="text-align:center;">
+                                    <h1 style="color:#1e1e2d; margin-top: 20px; margin-bottom:0; font-weight:bold; font-size:32px;font-family:'Rubik',sans-serif;">
+                                        Congratulations!</h1>
+                                    <h1 style="color:#1e1e2d; font-weight:bold; margin:0;font-size:48px;font-family:'Rubik',sans-serif;">
+                                        @isset($user_name)
+                                        {{ $user_name }}
+                                        @endisset</h1><br>
+                                     <h1 style="color:#1e1e2d; font-weight:bold; margin:0;font-size:40px;color:red;font-family:'Rubik',sans-serif;">You Have Earned</h1><br>
+                                     <h1 style="color:#1e1e2d; font-weight:bold; margin:0;font-size:60px;font-family:'Rubik',sans-serif;"> @isset($amount)
+                                        {{ $amount }}
+                                        @endisset INR</h1><br>
+                                </td>
+
+                            </tr>
+                            <!-- Details Table -->
+                            <tr>
+                                <td>
+                                    <p style="text-align:center;font-size:24px;font-weight:500;margin:5px 0 20px;">We Are Happy To Inform You That We Have Transferred Your Payment Into Your Bank Account.</p>
+                                    <h1 style="text-align:center;color:#1e1e2d; font-weight:bold; margin:0;font-size:36px;font-family:'Rubik',sans-serif;">
+                                        ({{date('d M Y')}})</h1><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:40px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+
+            </table>
         </td>
-      </tr>
-    </table>
-  </body>
+    </tr>
+</table><br>
+</body>
+
 </html>
