47437e9b5e6e35e626c4ceae5bdd1d7ac854e829
shaileshhq@gmail.com <shaileshhq@gmail.com>
2023-11-07 12:12:48 +0530

    payout padding updte

--- resources/views/email/payout.blade.php
+++ resources/views/email/payout.blade.php
@@ -25,7 +25,7 @@ a:hover {
                 <tr>
                     <td>
                         <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
-                            style="max-width:600px; background-image:url('{{ asset('frontend/images/email-bgs.jpg') }}'); border-radius:3px;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);padding:0 40px;">
+                            style="max-width:600px; background-image:url('{{ asset('frontend/images/email-bgs.jpg') }}'); border-radius:3px;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);padding:0 20px;">
                             <tr>
                                 <td style="height:40px;">&nbsp;</td>
                             </tr>
