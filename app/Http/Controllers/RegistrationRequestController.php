d063d5564e11142947259c3ec609ea8cee4f6db6
neelesh2210 <neelesh2210@gmail.com>
2023-12-14 10:23:56 +0530

    changes

--- app/Http/Controllers/RegistrationRequestController.php
+++ app/Http/Controllers/RegistrationRequestController.php
@@ -10,6 +10,7 @@ use App\Models\UserWallet;
 use App\Models\PlanPurchase;
 use Illuminate\Http\Request;
 use App\Models\Admin\UserDetail;
+use App\Models\WithdrawalRequest;
 use Illuminate\Support\Facades\Hash;
 use App\Models\WalletRegistrationRequest;
 use App\Http\Controllers\Auth\RegisterController;
@@ -59,79 +60,85 @@ class RegistrationRequestController extends Controller
             'email' => 'required|string|email|max:255|unique:users',
             'phone' => 'required|min:10|max:10|unique:users,phone',
         ]);
+        $withdrawal_request = WithdrawalRequest::where('user_id',Auth::guard('web')->user()->id)->latest()->first();
 
-        $reg_user = WalletRegistrationRequest::where('referral_code',Auth::guard('web')->user()->referrer_code)->where('id',$id)->first();
-        if($reg_user->status == 'pending'){
-            $plan = Plan::where('id',$reg_user->plan)->first();
-
-            if($plan->discounted_price <= Auth::guard('web')->user()->userDetail->total_wallet_balance){
-                $user = new User;
-                $user->name = $reg_user->name;
-                $user->email = $reg_user->email;
-                $user->phone = $reg_user->phone;
-                $user->state = $reg_user->state;
-                $user->referrer_code = 'RIND'.strtoupper(generateRandomString(6));
-                $user->referral_code = $reg_user->referral_code;
-                $user->password = Hash::make($reg_user->password);
-                $user->save();
-
-                $plan_purchase = new PlanPurchase;
-                $plan_purchase->user_id = $user->id;
-                $plan_purchase->plan_id = $plan->id;
-                $plan_purchase->amount = $plan->amount;
-                if($reg_user->referral_code){
-                    $total_amount = $plan->discounted_price;
-                    $plan_purchase->discounted_amount = $plan->amount - $plan->discounted_price;
-                }else{
-                    $total_amount = $plan->amount;
-                }
-
-                $plan_purchase->total_amount = $total_amount;
-                $plan_purchase->payment_detail = json_encode(['id'=>'From Wallet','amount'=>$plan->discounted_price]);
-                $plan_purchase->payment_status = 'success';
-                $plan_purchase->save();
-
-                $user_detail = new UserDetail;
-                $user_detail->user_id = $user->id;
-                $user_detail->current_plan_id = $plan->id;
-                $user_detail->save();
-
-                try{
-                    Mail::send('email.welcome_mail', ['user_name'=>$user], function($message) use ($user){
-                        $message->to($user->email);
-                        $message->subject('Welcome to Adsgyan');
-                    });
-                }catch (\Throwable $th) {
-
-                }
-
-                if($reg_user->referral_code){
-                    $register_controller = new RegisterController;
-                    $register_controller->distributeCommission($reg_user->referral_code,$plan->id,$plan_purchase->id,$user);
-                }
-
-                $reg_user->status = 'success';
-                $reg_user->save();
-
-                $user_wallet = new UserWallet;
-                $user_wallet->user_id = Auth::guard('web')->user()->id;
-                $user_wallet->from_id = $plan_purchase->id;
-                $user_wallet->amount = $total_amount;
-                $user_wallet->type = 'debit';
-                $user_wallet->from = 'register';
-                $user_wallet->save();
-
-                $user_detail = UserDetail::where('user_id',Auth::guard('web')->user()->id)->first();
-                $user_detail->total_wallet_balance = $user_detail->total_wallet_balance - $total_amount;
-                $user_detail->save();
-
-                return redirect()->route('user.registration.request')->with('success','User Register Successfully!');
-            }else{
-                return back()->with('error','You Have Insufficient Balance!');
-            }
+        if($withdrawal_request->status == 'pending'){
+            return back()->with('error','You Have Pending Withdrawal Request!');
         }else{
-            return back()->with('error','This Request Already Proccessed!');
+            $reg_user = WalletRegistrationRequest::where('referral_code',Auth::guard('web')->user()->referrer_code)->where('id',$id)->first();
+            if($reg_user->status == 'pending'){
+                $plan = Plan::where('id',$reg_user->plan)->first();
+
+                if($plan->discounted_price <= Auth::guard('web')->user()->userDetail->total_wallet_balance){
+                    $user = new User;
+                    $user->name = $reg_user->name;
+                    $user->email = $reg_user->email;
+                    $user->phone = $reg_user->phone;
+                    $user->state = $reg_user->state;
+                    $user->referrer_code = 'RIND'.strtoupper(generateRandomString(6));
+                    $user->referral_code = $reg_user->referral_code;
+                    $user->password = Hash::make($reg_user->password);
+                    $user->save();
+
+                    $plan_purchase = new PlanPurchase;
+                    $plan_purchase->user_id = $user->id;
+                    $plan_purchase->plan_id = $plan->id;
+                    $plan_purchase->amount = $plan->amount;
+                    if($reg_user->referral_code){
+                        $total_amount = $plan->discounted_price;
+                        $plan_purchase->discounted_amount = $plan->amount - $plan->discounted_price;
+                    }else{
+                        $total_amount = $plan->amount;
+                    }
+
+                    $plan_purchase->total_amount = $total_amount;
+                    $plan_purchase->payment_detail = json_encode(['id'=>'From Wallet','amount'=>$plan->discounted_price]);
+                    $plan_purchase->payment_status = 'success';
+                    $plan_purchase->save();
+
+                    $user_detail = new UserDetail;
+                    $user_detail->user_id = $user->id;
+                    $user_detail->current_plan_id = $plan->id;
+                    $user_detail->save();
+
+                    try{
+                        Mail::send('email.welcome_mail', ['user_name'=>$user], function($message) use ($user){
+                            $message->to($user->email);
+                            $message->subject('Welcome to Adsgyan');
+                        });
+                    }catch (\Throwable $th) {
+
+                    }
+
+                    if($reg_user->referral_code){
+                        $register_controller = new RegisterController;
+                        $register_controller->distributeCommission($reg_user->referral_code,$plan->id,$plan_purchase->id,$user);
+                    }
+
+                    $reg_user->status = 'success';
+                    $reg_user->save();
+
+                    $user_wallet = new UserWallet;
+                    $user_wallet->user_id = Auth::guard('web')->user()->id;
+                    $user_wallet->from_id = $plan_purchase->id;
+                    $user_wallet->amount = $total_amount;
+                    $user_wallet->type = 'debit';
+                    $user_wallet->from = 'register';
+                    $user_wallet->save();
+
+                    $user_detail = UserDetail::where('user_id',Auth::guard('web')->user()->id)->first();
+                    $user_detail->total_wallet_balance = $user_detail->total_wallet_balance - $total_amount;
+                    $user_detail->save();
+
+                    return redirect()->route('user.registration.request')->with('success','User Register Successfully!');
+                }else{
+                    return back()->with('error','You Have Insufficient Balance!');
+                }
+            }else{
+                return back()->with('error','This Request Already Proccessed!');
+            }
         }
+
 
     }
 
