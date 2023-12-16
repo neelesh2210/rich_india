65acc17c07a22d8027fd9bb7c5c4ae7f678261d3
neelesh2210 <neelesh2210@gmail.com>
2023-12-16 16:10:28 +0530

    changes

--- app/Http/Controllers/UpgradePlanRequestController.php
+++ app/Http/Controllers/UpgradePlanRequestController.php
@@ -53,88 +53,166 @@ class UpgradePlanRequestController extends Controller
 
     public function update(Request $request,$id){
         $withdrawal_request = WithdrawalRequest::where('user_id',Auth::guard('web')->user()->id)->latest()->first();
-        if($withdrawal_request->status == 'pending'){
+        if(optional($withdrawal_request)->status == 'pending'){
             return back()->with('error','You Have Pending Withdrawal Request!');
         }else{
             $data = UpgradePlanRequest::with(['user','plan'])->findOrFail(decrypt($id));
-            if($data->status == 'pending'){
-                $user_detail = UserDetail::where('user_id',$data->user_id)->first();
-                $current_plan = Plan::where('id',$user_detail->current_plan_id)->first();
-                $upgrade_plan_detail = Plan::where('id',$data->plan_id)->first();
-                $total_amount = $current_plan->upgrade_amount[$upgrade_plan_detail->priority - $current_plan->priority - 1];
-                if($total_amount <= Auth::guard('web')->user()->userDetail->total_wallet_balance){
-                    $plan_purchase = new PlanPurchase;
-                    $plan_purchase->user_id = $data->user_id;
-                    $plan_purchase->plan_id = $data->plan_id;
-                    $plan_purchase->amount = $total_amount;
-                    $plan_purchase->total_amount = $total_amount;
-                    $plan_purchase->payment_detail = json_encode(["id"=>"From Wallet","amount"=>$total_amount]);
-                    $plan_purchase->payment_status = 'success';
-                    $plan_purchase->save();
-
-                    $user_wallet = new UserWallet;
-                    $user_wallet->user_id = Auth::guard('web')->user()->id;
-                    $user_wallet->from_id = $plan_purchase->id;
-                    $user_wallet->amount = $total_amount;
-                    $user_wallet->type = 'debit';
-                    $user_wallet->from = 'upgrade';
-                    $user_wallet->save();
-
-                    $user_detail->current_plan_id = $data->plan_id;
-                    $user_detail->save();
-
-                    $upgrade_user_detail = UserDetail::where('user_id',Auth::guard('web')->user()->id)->first();
-                    $upgrade_user_detail->total_wallet_balance = $upgrade_user_detail->total_wallet_balance - $total_amount;
-                    $upgrade_user_detail->save();
-
-                    $upgrade_commission_level = WebsiteSetting::where('type','upgrade_commission_level')->first();
-
-                    $referrer_code = Auth::guard('web')->user()->referrer_code;
-
-                    for($i=1;$i<=$upgrade_commission_level->content;$i++){
-                        $commission_user = User::where('referrer_code',$referrer_code)->first();
-                        if($commission_user){
-                            $update_commission = UserDetail::where('user_id',$commission_user->id)->first();
-                            $referrer_code = optional(User::where('id',$update_commission->user_id)->first())->referral_code;
-                        }else{
-                            $update_commission = UserDetail::first();
-                            $referrer_code = User::where('id',$update_commission->user_id)->first()->referrer_code;
-                        }
-
-                        $commission = new Commission;
-                        $commission->user_id = $update_commission->user_id;
-                        $commission->plan_purchase_id = $plan_purchase->id;
-                        $commission->commission = $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
-                        $commission->level = $i;
-                        $commission->save();
+            if($data){
+                if($data->status == 'pending'){
+                    $user_detail = UserDetail::where('user_id',$data->user_id)->first();
+                    $current_plan = Plan::where('id',$user_detail->current_plan_id)->first();
+                    $upgrade_plan_detail = Plan::where('id',$data->plan_id)->first();
+                    $total_amount = $current_plan->upgrade_amount[$upgrade_plan_detail->priority - $current_plan->priority - 1];
+                    if($total_amount <= Auth::guard('web')->user()->userDetail->total_wallet_balance){
+                        $plan_purchase = new PlanPurchase;
+                        $plan_purchase->user_id = $data->user_id;
+                        $plan_purchase->plan_id = $data->plan_id;
+                        $plan_purchase->amount = $total_amount;
+                        $plan_purchase->total_amount = $total_amount;
+                        $plan_purchase->payment_detail = json_encode(["id"=>"From Wallet","amount"=>$total_amount]);
+                        $plan_purchase->payment_status = 'success';
+                        $plan_purchase->save();
 
                         $user_wallet = new UserWallet;
-                        $user_wallet->user_id = $update_commission->user_id;
+                        $user_wallet->user_id = Auth::guard('web')->user()->id;
                         $user_wallet->from_id = $plan_purchase->id;
-                        $user_wallet->amount = $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
-                        $user_wallet->type = 'credit';
-                        if($i==1){
-                            $user_wallet->from = 'active_commission';
-                        }else{
-                            $user_wallet->from = 'passive_commission';
-                        }
+                        $user_wallet->amount = $total_amount;
+                        $user_wallet->type = 'debit';
+                        $user_wallet->from = 'upgrade';
                         $user_wallet->save();
 
-                        $update_commission->total_commission = $update_commission->total_commission + $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
-                        $update_commission->total_wallet_balance = $update_commission->total_wallet_balance + $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
-                        $update_commission->save();
+                        $user_detail->current_plan_id = $data->plan_id;
+                        $user_detail->save();
 
+                        $upgrade_user_detail = UserDetail::where('user_id',Auth::guard('web')->user()->id)->first();
+                        $upgrade_user_detail->total_wallet_balance = $upgrade_user_detail->total_wallet_balance - $total_amount;
+                        $upgrade_user_detail->save();
+
+                        $upgrade_commission_level = WebsiteSetting::where('type','upgrade_commission_level')->first();
+
+                        $referrer_code = Auth::guard('web')->user()->referrer_code;
+
+                        for($i=1;$i<=$upgrade_commission_level->content;$i++){
+                            $commission_user = User::where('referrer_code',$referrer_code)->first();
+                            if($commission_user){
+                                $update_commission = UserDetail::where('user_id',$commission_user->id)->first();
+                                $referrer_code = optional(User::where('id',$update_commission->user_id)->first())->referral_code;
+                            }else{
+                                $update_commission = UserDetail::first();
+                                $referrer_code = User::where('id',$update_commission->user_id)->first()->referrer_code;
+                            }
+
+                            $commission = new Commission;
+                            $commission->user_id = $update_commission->user_id;
+                            $commission->plan_purchase_id = $plan_purchase->id;
+                            $commission->commission = $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
+                            $commission->level = $i;
+                            $commission->save();
+
+                            $user_wallet = new UserWallet;
+                            $user_wallet->user_id = $update_commission->user_id;
+                            $user_wallet->from_id = $plan_purchase->id;
+                            $user_wallet->amount = $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
+                            $user_wallet->type = 'credit';
+                            if($i==1){
+                                $user_wallet->from = 'active_commission';
+                            }else{
+                                $user_wallet->from = 'passive_commission';
+                            }
+                            $user_wallet->save();
+
+                            $update_commission->total_commission = $update_commission->total_commission + $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
+                            $update_commission->total_wallet_balance = $update_commission->total_wallet_balance + $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
+                            $update_commission->save();
+
+                        }
+
+                        $data->status = 'success';
+                        $data->save();
+
+                        return redirect()->route('user.upgrade.plan.request.list')->with('success','Plan Upgrade Successfully!');
+                    }else{
+                        return back()->with('error','Insufficient Balance!');
                     }
-
-                    $data->status = 'success';
-                    $data->save();
-
-                    return redirect()->route('user.upgrade.plan.request.list')->with('success','Plan Upgrade Successfully!');
                 }else{
-                    return back()->with('error','Insufficient Balance!');
+                    return back()->with('error','This Request Already Proccessed!');
                 }
             }else{
-                return back()->with('error','This Request Already Proccessed!');
+                $user_detail = UserDetail::where('user_id',$data->user_id)->first();
+                    $current_plan = Plan::where('id',$user_detail->current_plan_id)->first();
+                    $upgrade_plan_detail = Plan::where('id',$data->plan_id)->first();
+                    $total_amount = $current_plan->upgrade_amount[$upgrade_plan_detail->priority - $current_plan->priority - 1];
+                    if($total_amount <= Auth::guard('web')->user()->userDetail->total_wallet_balance){
+                        $plan_purchase = new PlanPurchase;
+                        $plan_purchase->user_id = $data->user_id;
+                        $plan_purchase->plan_id = $data->plan_id;
+                        $plan_purchase->amount = $total_amount;
+                        $plan_purchase->total_amount = $total_amount;
+                        $plan_purchase->payment_detail = json_encode(["id"=>"From Wallet","amount"=>$total_amount]);
+                        $plan_purchase->payment_status = 'success';
+                        $plan_purchase->save();
+
+                        $user_wallet = new UserWallet;
+                        $user_wallet->user_id = Auth::guard('web')->user()->id;
+                        $user_wallet->from_id = $plan_purchase->id;
+                        $user_wallet->amount = $total_amount;
+                        $user_wallet->type = 'debit';
+                        $user_wallet->from = 'upgrade';
+                        $user_wallet->save();
+
+                        $user_detail->current_plan_id = $data->plan_id;
+                        $user_detail->save();
+
+                        $upgrade_user_detail = UserDetail::where('user_id',Auth::guard('web')->user()->id)->first();
+                        $upgrade_user_detail->total_wallet_balance = $upgrade_user_detail->total_wallet_balance - $total_amount;
+                        $upgrade_user_detail->save();
+
+                        $upgrade_commission_level = WebsiteSetting::where('type','upgrade_commission_level')->first();
+
+                        $referrer_code = Auth::guard('web')->user()->referrer_code;
+
+                        for($i=1;$i<=$upgrade_commission_level->content;$i++){
+                            $commission_user = User::where('referrer_code',$referrer_code)->first();
+                            if($commission_user){
+                                $update_commission = UserDetail::where('user_id',$commission_user->id)->first();
+                                $referrer_code = optional(User::where('id',$update_commission->user_id)->first())->referral_code;
+                            }else{
+                                $update_commission = UserDetail::first();
+                                $referrer_code = User::where('id',$update_commission->user_id)->first()->referrer_code;
+                            }
+
+                            $commission = new Commission;
+                            $commission->user_id = $update_commission->user_id;
+                            $commission->plan_purchase_id = $plan_purchase->id;
+                            $commission->commission = $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
+                            $commission->level = $i;
+                            $commission->save();
+
+                            $user_wallet = new UserWallet;
+                            $user_wallet->user_id = $update_commission->user_id;
+                            $user_wallet->from_id = $plan_purchase->id;
+                            $user_wallet->amount = $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
+                            $user_wallet->type = 'credit';
+                            if($i==1){
+                                $user_wallet->from = 'active_commission';
+                            }else{
+                                $user_wallet->from = 'passive_commission';
+                            }
+                            $user_wallet->save();
+
+                            $update_commission->total_commission = $update_commission->total_commission + $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
+                            $update_commission->total_wallet_balance = $update_commission->total_wallet_balance + $current_plan->upgrade_commission[$upgrade_plan_detail->priority - $current_plan->priority - 1][$i-1];
+                            $update_commission->save();
+
+                        }
+
+                        $data->status = 'success';
+                        $data->save();
+
+                        return redirect()->route('user.upgrade.plan.request.list')->with('success','Plan Upgrade Successfully!');
+                    }else{
+                        return back()->with('error','Insufficient Balance!');
+                    }
             }
         }
 
