fff12834b9c48507baca7c0b38a86fa7b774be67
shaileshhq@gmail.com <shaileshhq@gmail.com>
2024-01-02 12:41:49 +0530

    updete policy

--- resources/views/frontend/cancel_and_refund_policy.blade.php
+++ resources/views/frontend/cancel_and_refund_policy.blade.php
@@ -18,9 +18,9 @@
                         <div class="terms-text">
                             <div class="col-md-12">
                                 <h3>Overview </h3>
-                                <p>Our refund and returns policy lasts 2 hours. If 2 hours have passed since your purchase,
+                                <p>Our refund and returns policy lasts 24 hours. If 24 hours have passed since your purchase,
                                     we can’t offer you a full refund or exchange.</p>
-                                {{-- <p>A payment gateway fee @ 2% of the paid amount and processing fee @5% of the paid amount will be deducted from the amount to be refunded.</p> --}}
+                                <p>Amount will be refunded after deducting Payment Gateway charges.</p>
                                 <p>No Refund will be given to the customer for the purchase of any package made by the
                                     customer directly from the RichIND website “{{ env('APP_URL') }}” or through the
                                     affiliate link of the person who referred him to the RichIND website after 2
@@ -28,7 +28,7 @@
                                 <p>To complete your refund, we require a receipt or proof of purchase.</p>
 
                                 <h3>Cancel & Refund Policy</h3>
-                                <p>Our Cancel and refund policy lasts 2 hours if you have not viewed course. If 2 hours have
+                                <p>Our Cancel and refund policy lasts 24 hours if you have not viewed course. If 24 hours have
                                     passed since your purchase, we can’t offer you a full refund.</p>
                                 <p>To be eligible for a refund your course must be unseen.</p>
                                 <p>To complete your return, we require a receipt or proof of purchase.</p>
@@ -50,15 +50,15 @@
                                 <p>Next contact your bank. There is often some processing time before a refund is posted.
                                 </p>
                                 <p>If you’ve done all of this and you still have not received your refund yet, please
-                                    contact us at info@careefix.com.</p>
+                                    contact us at info@richind.in.</p>
                                 <br><br>
-                                <p>For the refund, you need to mail at info@careefix.com In the following format
+                                <p>For the refund, you need to mail at info@richind.in In the following format
                                     with registered e-mail ID only.</p>
                                 <p>Full Name –</p>
                                 <p>Registered e-mail ID –</p>
                                 <p>Registration date –</p>
                                 <h3>Need Help?</h3>
-                                <p>Contact us at info@careefix.com for questions related to refunds.</p>
+                                <p>Contact us at info@richind.in for questions related to refunds.</p>
                             </div>
                         </div>
                     </div>
