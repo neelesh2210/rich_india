922546069d52c8c7c92ceed18944e73769bb2351
Shailesh Gupta <shaileshhq@gmail.com>
2024-09-09 14:55:19 +0530

    updte checkout login, forget pass

--- resources/views/frontend/forgot_password.blade.php
+++ resources/views/frontend/forgot_password.blade.php
@@ -1,38 +1,49 @@
 @extends('frontend.layouts.app')
 @section('content')
-    <!-- <section class="page-header page-header--bg-two" data-jarallax data-speed="0.3" data-imgPosition="50% -100%">
-        <div class="page-header__bg jarallax-img"></div>
-        <div class="page-header__overlay"></div>
-        <div class="container text-center">
-            <h2 class="page-header__title">Forgot Password</h2>
-            <ul class="page-header__breadcrumb list-unstyled">
-                <li><a href="{{ route('index') }}">Home</a></li>
-                <li><span>Forgot Password</span></li>
-            </ul>
-        </div>
-    </section> -->
-    <section class="login-page">
+     <!-- breadcrumb-area -->
+     <section class="breadcrumb__area breadcrumb__bg" data-background="{{ asset('frontend/assets/img/bg/breadcrumb_bg.jpg')}}">
         <div class="container">
-            <div class="login-page__area">
-                <div class="row">
-                    <div class="col-lg-6 wow fadeInUp animated" data-wow-delay="400ms">
-                        <div class="login-page__wrap login-page__wrap--right">
-                            <img src="{{ asset('frontend/assets/images/reset-password.jpg') }}" alt="Alternate Text"
-                                class="img-fluid" />
-                        </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumb__content">
+                        <h3 class="title">Login</h3>
+                        <nav class="breadcrumb">
+                            <span property="itemListElement" typeof="ListItem">
+                                <a href="index.html">Home</a>
+                            </span>
+                            <span class="breadcrumb-separator"><i class="fas fa-angle-right"></i></span>
+                            <span property="itemListElement" typeof="ListItem">Login</span>
+                        </nav>
                     </div>
-                    <div class="col-lg-6 wow fadeInUp animated" data-wow-delay="300ms">
-                        <div class="login-page__wrap">
-                            <h3 class="login-page__wrap__title">Forgot Password</h3>
-                            <form action="{{ route('send.mail.forgot.password') }}" method="POST"  class="login-page__form">
+                </div>
+            </div>
+        </div>
+        <div class="breadcrumb__shape-wrap">
+            <img src="{{ asset('frontend/assets/img/others/breadcrumb_shape01.svg')}}" alt="img" class="alltuchtopdown">
+            <img src="{{ asset('frontend/assets/img/others/breadcrumb_shape02.svg')}}" alt="img" data-aos="fade-right" data-aos-delay="300">
+            <img src="{{ asset('frontend/assets/img/others/breadcrumb_shape03.svg')}}" alt="img" data-aos="fade-up" data-aos-delay="400">
+            <img src="{{ asset('frontend/assets/img/others/breadcrumb_shape04.svg')}}" alt="img" data-aos="fade-down-left" data-aos-delay="400">
+            <img src="{{ asset('frontend/assets/img/others/breadcrumb_shape05.svg')}}" alt="img" data-aos="fade-left" data-aos-delay="400">
+        </div>
+    </section>
+    <!-- breadcrumb-area-end -->
+
+        <!-- Forget-area -->
+        <section class="singUp-area section-py-120">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6 col-lg-8">
+                        <div class="singUp-wrap">
+                            <h2 class="title text-center">Welcome RichInd!</h2>
+                            <p class="text-center">Forgot Password!</p>
+                            <form action="{{ route('send.mail.forgot.password') }}" method="POST"  class="account__form">
                                 @csrf
                                     @if ($errors->has('error'))
                                         <div class="text-danger">{{ $errors->first('error') }}</div>
                                     @endif
                                     <div>
-                                        <div class="login-page__form-input-box">
-                                            <input type="email" name="email" class="form-control"
-                                                placeholder="Email Id" />
+                                        <div class="form-grp">
+                                            <input type="email" name="email" placeholder="Email Id" />
                                             @if ($errors->has('email'))
                                                 <div class="text-danger lbl_msg">{{ $errors->first('email') }}</div>
                                             @endif
@@ -48,21 +59,16 @@
                                                     RichIND</a></span>
                                         </div>
                                         <div class="d-grid mt-2">
-                                            <button type="submit" class="richind-btn richind-btn-second w-100"><span
+                                            <button type="submit" class="btn btn-two arrow-btn w-100"><span
                                                     class="richind-btn__curve"></span>Submit</button>
                                         </div>
                                     </div>
-                                    {{-- <div class="col-md-12 col-sm-12 col-xs-12">
-                                                    <div class="account-text">
-                                                        <p>Don’t have an account? <a href="{{route('signup')}}">Register now</a></p>
-                                                    </div>
-                                                </div> --}}
-
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+        <!-- Forget-area-end -->
+
 @endsection
--- resources/views/frontend/signin.blade.php
+++ resources/views/frontend/signin.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="breadcrumb__content">
-                        <h3 class="title">Student Login</h3>
+                        <h3 class="title">Login</h3>
                         <nav class="breadcrumb">
                             <span property="itemListElement" typeof="ListItem">
                                 <a href="index.html">Home</a>
@@ -36,34 +36,81 @@
                     <div class="singUp-wrap">
                         <h2 class="title text-center">Welcome back!</h2>
                         <p class="text-center">Hey there! Ready to log in? Just enter your username and password below and you'll be back in action in no time. Let's go!</p>
-
-                        <form action="#" class="account__form">
-                            <div class="form-grp">
-                                <label for="email">Email</label>
-                                <input id="email" type="text" placeholder="email">
-                            </div>
-                            <div class="form-grp">
-                                <label for="password">Password</label>
-                                <input id="password" type="text" placeholder="password">
+                        <form action="{{ route('login') }}" method="POST" class="account__form" id="login-form">
+                            @csrf
+                            @if ($errors->has('error'))
+                                <div class="text-danger">{{ $errors->first('error') }}</div>
+                            @endif
+                            <div>
+                                <div class="form-grp">
+                                    <input type="email" name="email" placeholder="Email Id" />
+                                    @if ($errors->has('email'))
+                                        <div class="text-danger lbl_msg">{{ $errors->first('email') }}</div>
+                                    @endif
+                                </div>
+                                <div class="form-grp">
+                                    <div class="input-group" id="show_hide_password">
+                                        <input type="password" name="password" id="password"
+                                            value="" placeholder="Enter Password..." autocomplete="new-password">
+                                        <div class="input-group-append">
+                                            <div class="input-group-text">
+                                                <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    @if ($errors->has('password'))
+                                        <div class="text-danger lbl_msg">{{ $errors->first('password') }}</div>
+                                    @endif
+                                </div>
+                                {{-- <div class="form-group">
+                                    <label class="form-control-label">Password</label>
+                                    <input type="password" class="form-control" name="password" id="password"
+                                    value="" placeholder="Enter your Password" autocomplete="new-password">
+                                    @if ($errors->has('password'))
+                                    <div class="text-danger lbl_msg">{{ $errors->first('password') }}</div>
+                                     @endif
+                                </div> --}}
                             </div>
                             <div class="account__check">
                                 <div class="account__check-remember">
-                                    <input type="checkbox" class="form-check-input" value="" id="terms-check">
-                                    <label for="terms-check" class="form-check-label">Remember me</label>
+                                    <input type="checkbox" name="save-data" class="form-check-input" id="save-data">
+                                    <label for="save-data" class="form-check-label">Remember me</label>
                                 </div>
                                 <div class="account__check-forgot">
-                                    <a href="#">Forgot Password?</a>
+                                    <a href="{{ route('forgot.password') }}">Forgot Password?</a>
                                 </div>
                             </div>
-                            <button type="submit" class="btn btn-two arrow-btn">Sign In<img src="{{ asset('frontend/assets/img/icons/right_arrow.svg')}}" alt="img" class="injectable"></button>
+                            <button type="button" id="login_button_id" class="btn btn-two arrow-btn w-100" onclick="submitLoginForm()"><span class="richind-btn__curve"></span>Login <i class="icon-arrow"></i></button>
                         </form>
-                        <div class="account__switch">
-                            <p>Don't have an account?<a href="#">Sign Up</a></p>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
     <!-- singUp-area-end -->
+
+
+    @push('js')
+    <script>
+        $(document).ready(function() {
+            $("#show_hide_password a").on('click', function(event) {
+                event.preventDefault();
+                if ($('#show_hide_password input').attr("type") == "text") {
+                    $('#show_hide_password input').attr('type', 'password');
+                    $('#show_hide_password i').addClass("fa-eye-slash");
+                    $('#show_hide_password i').removeClass("fa-eye");
+                } else if ($('#show_hide_password input').attr("type") == "password") {
+                    $('#show_hide_password input').attr('type', 'text');
+                    $('#show_hide_password i').removeClass("fa-eye-slash");
+                    $('#show_hide_password i').addClass("fa-eye");
+                }
+            });
+        });
+
+        function submitLoginForm(){
+            $('#login_button_id').attr('disabled',true);
+            $('#login-form').submit();
+        }
+    </script>
+@endpush
 @endsection
