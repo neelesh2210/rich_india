9ca0b498ff6dfa1a967ce97d9722fce0a317130d
shaileshhq <shaileshhq@gmail.com>
2025-03-26 12:06:41 +0530

    Enhance 500 error page with improved styling and layout for better user experience

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,163 +1,216 @@
-<style>
-    @use postcss-cssnext;
-    /* helpers/align.css */
-
-    .align {
-        align-items: center;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-    }
-
-    /* helpers/animation.css */
-
-    .animation {
-        animation-duration: 0.6s;
-        animation-fill-mode: both;
-    }
-
-    .animation--reverse {
-        animation-direction: reverse;
-    }
-
-    /* helpers/animation-shake.css */
-
-    @keyframes shake-vertical {
-
-        15%,
-        45%,
-        75% {
-            transform: translateY(-0.75rem);
-        }
-
-        30%,
-        60% {
-            transform: translateY(0.75rem);
-        }
-
-    }
-
-    .animation--shake--vertical {
-        animation-name: shake-vertical;
-        will-change: transform;
-    }
-
-    :root {
-        --grid-max-width: 40rem;
-        --grid-width: 90%;
-        --grid-gutter: 3%;
-    }
-
-    .grid {
-        margin-left: auto;
-        margin-right: auto;
-        max-width: var(--grid-max-width);
-        width: var(--grid-width);
-    }
-
-    .grid__row {
-        display: flex;
-        flex-wrap: wrap;
-        margin: calc(var(--grid-gutter) / -2);
-    }
-
-    .grid__col {
-        margin: calc(var(--grid-gutter) / 2);
-        flex-grow: 1;
-    }
-
-    /* layout/base.css */
-
-    :root {
-        --html-font-size: 100%;
-
-        --body-background-color: #f9f8f5;
-        --body-color: #353535;
-        --body-font-family: 'Roboto';
-        --body-font-family-fallback: sans-serif;
-        --body-font-size: 1rem;
-        --body-font-weight: 400;
-        --body-line-height: 1.5;
-    }
-
-    *,
-    *::before,
-    *::after {
-        box-sizing: inherit;
-    }
-
-    html {
-        box-sizing: border-box;
-        font-size: var(--html-font-size);
-        height: 100%;
-    }
-
-    body {
-        background-color: var(--body-background-color);
-        color: var(--body-color);
-        font-family: var(--body-font-family), var(--body-font-family-fallback);
-        font-size: var(--body-font-size);
-        font-weight: var(--body-font-weight);
-        line-height: var(--body-line-height);
-        margin: 0;
-        min-height: 100%;
-    }
-
-    /* modules/box.css */
-
-    :root {
-        --box-background-color: #fff;
-        --box-padding-horizontal: 2rem;
-        --box-padding-vertical: 3rem;
-    }
-
-    .box {
-        background-color: var(--box-background-color);
-        box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.3);
-        font-size: 6.25rem;
-        padding-bottom: var(--box-padding-vertical);
-        padding-left: var(--box-padding-horizontal);
-        padding-right: var(--box-padding-horizontal);
-        padding-top: var(--box-padding-vertical);
-        text-align: center;
-    }
-</style>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
-    <title>500</title>
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>500 - Internal Server Error</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <style>
+        * {
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+        }
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400" rel="stylesheet">
+        body {
+            padding: 0;
+            margin: 0;
+        }
 
+        #notfound {
+            position: relative;
+            height: 100vh;
+        }
+
+        #notfound .notfound {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+
+        #notfound .notfound-bg {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            overflow: hidden;
+        }
+
+        #notfound .notfound-bg>div {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            width: 1px;
+            background-color: #eee;
+        }
+
+        #notfound .notfound-bg>div:nth-child(1) {
+            left: 20%;
+        }
+
+        #notfound .notfound-bg>div:nth-child(2) {
+            left: 40%
+        }
+
+        #notfound .notfound-bg>div:nth-child(3) {
+            left: 60%
+        }
+
+        #notfound .notfound-bg>div:nth-child(4) {
+            left: 80%
+        }
+
+        #notfound .notfound-bg>div:after {
+            content: '';
+            position: absolute;
+            top: 0px;
+            left: -0.5px;
+            -webkit-transform: translateY(-160px);
+            -ms-transform: translateY(-160px);
+            transform: translateY(-160px);
+            height: 160px;
+            width: 2px;
+            background-color: #5751e1;
+        }
+
+        @-webkit-keyframes drop {
+            90% {
+                height: 20px;
+            }
+
+            100% {
+                height: 160px;
+                -webkit-transform: translateY(calc(100vh + 160px));
+                transform: translateY(calc(100vh + 160px));
+            }
+        }
+
+        @keyframes drop {
+            90% {
+                height: 20px;
+            }
+
+            100% {
+                height: 160px;
+                -webkit-transform: translateY(calc(100vh + 160px));
+                transform: translateY(calc(100vh + 160px));
+            }
+        }
+
+        #notfound .notfound-bg>div:nth-child(1):after {
+            -webkit-animation: drop 3s infinite linear;
+            animation: drop 3s infinite linear;
+            -webkit-animation-delay: 0.2s;
+            animation-delay: 0.2s;
+        }
+
+        #notfound .notfound-bg>div:nth-child(2):after {
+            -webkit-animation: drop 2s infinite linear;
+            animation: drop 2s infinite linear;
+            -webkit-animation-delay: 0.7s;
+            animation-delay: 0.7s;
+        }
+
+        #notfound .notfound-bg>div:nth-child(3):after {
+            -webkit-animation: drop 3s infinite linear;
+            animation: drop 3s infinite linear;
+            -webkit-animation-delay: 0.9s;
+            animation-delay: 0.9s;
+        }
+
+        #notfound .notfound-bg>div:nth-child(4):after {
+            -webkit-animation: drop 2s infinite linear;
+            animation: drop 2s infinite linear;
+            -webkit-animation-delay: 1.2s;
+            animation-delay: 1.2s;
+        }
+
+        .notfound {
+            max-width: 520px;
+            width: 100%;
+            text-align: center;
+        }
+
+        .notfound .notfound-404 {
+            height: 210px;
+            line-height: 210px;
+        }
+
+        .notfound .notfound-404 h1 {
+            font-family: "Poppins", sans-serif;
+            font-size: 188px;
+            font-weight: 700;
+            margin: 0px;
+            text-shadow: 4px 4px 0px #ffc224;
+        }
+
+        .notfound h2 {
+            font-family: "Poppins", sans-serif;
+            font-size: 42px;
+            font-weight: 700;
+            margin: 0;
+            text-transform: uppercase;
+            letter-spacing: 1.6px;
+        }
+
+        .notfound p {
+            font-family: "Poppins", sans-serif;
+            color: #000;
+            font-weight: 400;
+            margin-top: 20px;
+            margin-bottom: 25px;
+        }
+
+        .notfound a {
+            font-family: "Poppins", sans-serif;
+            padding: 10px 30px;
+            display: inline-block;
+            color: #fff;
+            font-weight: 600;
+            text-transform: uppercase;
+            background: #5751e1;
+            box-shadow: 4px 6px 0px 0px #050071;
+            text-decoration: none;
+            -webkit-transition: 0.2s all;
+            transition: 0.2s all;
+            border-radius: 50px;
+            letter-spacing: 1px;
+        }
+
+        .notfound a:hover {
+            color: #000;
+            background-color: #ffc224;
+            -webkit-box-shadow: 0px 0px 0px 0px #000, 0px 0px 0px 2px #ffc224;
+            box-shadow: 0px 0px 0px 0px #000, 0px 0px 0px 2px #ffc224;
+        }
+    </style>
 </head>
 
-<body class="align">
-
-    <div class="grid">
-
-        <div class="grid__row">
-
-            <div class="grid__col">
-                <div class="box animation animation--shake--vertical">5</div>
-            </div>
-
-            <div class="grid__col">
-                <div class="box animation animation--reverse animation--shake--vertical">0</div>
-            </div>
-
-            <div class="grid__col">
-                <div class="box animation animation--shake--vertical">0</div>
-            </div>
-
+<body>
+    <div id="notfound">
+        <div class="notfound-bg">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
-
+        <div class="notfound">
+            <div class="notfound-404">
+                <h1>500</h1>
+            </div>
+            <h2>Internal Server Error</h2>
+            <p>Oops! Something went wrong on our end. Please try again later or go back to the homepage.
+            </p>
+            <a href="/">Homepage</a>
+        </div>
     </div>
-
 </body>
 
 </html>
